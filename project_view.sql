b0aefe5f7cb83cd5f47b66ac95b453996634084e
Mark Jumba <jumbamark@yahoo.com>
2023-07-05 11:01:12 +0300

    Add project view t
    To be used in tableau for visualizations

---
+++ project_view.sql
@@ -0,0 +1,9 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
+
+  SELECT * FROM PercentPopulationVaccinated
